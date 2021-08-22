7169ed93c2c378cff1079b74b8c0ff7411dbcf1c
IvanSJr <ivan.junior2706@gmail.com>
2021-08-22 03:19:01 -0300

    refactor: Melhoria no casting de List para ListDTO de User.

--- src/main/java/com/navi/springmongo/resources/UserResource.java
+++ src/main/java/com/navi/springmongo/resources/UserResource.java
@@ -19,6 +19,7 @@ public class UserResource {
     @Autowired
     private UserService service;
 
+    
     @GetMapping
     public ResponseEntity<List<UserDTO>> findAll(){
         List<User> list = service.findAll();
